248b9a7025fe65aa58d8c5a0e3bd9bf9003b04bf
mitchelle <mitchellenjoki7@gmail.com>
2022-06-25 13:22:57 +0300

    movies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-const init = () => {
-  
+let url = "http://localhost:3000/movies";
+document.addEventListener("DOMContentLoaded", () => {
+  getFetch();
+  formData();
+});
+
+function getFetch() {
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((mariam) => console.log(mariam));
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
+function formData() {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", () => {});
+}
